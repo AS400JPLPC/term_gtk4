10e4a8aa4d69ff76c7c00ee6e7af5f70d4b9025c
AS400JPLPC <laroche.jeanpierre@gmail.com>
2025-12-20 23:21:07 +0100

    maj_20251220-23:20

---
+++ termapp/TermApp.cpp
@@ -0,0 +1,430 @@
+/// -----------------------------------------------------------------
+/// Jean-Pierre Laroche
+/// projet 2018-08-08  (C) 2018   Copyright 2018  <laroche.jeanpierre@gmail.com>
+/// but :
+///			fast / flexible terminal / respecting the escape code
+///			-no-pie EXÉCUTABLE  programme server terminal
+///			More flexible than XTERM and more secure than public terminals
+///			tool to develop a 5250/3270 or terminal semi-graphic application
+
+
+#include <filesystem>
+#include <sys/stat.h>
+#include <X11/Xlib.h>
+
+
+
+
+#include <vte/vte.h>
+#include <adwaita.h>
+#include <glib/gprintf.h>
+#include <gtk-4.0/gdk/gdk.h>
+
+///---------------------
+/// paramétrage spécifique
+/// ex:
+///------------------------------------------
+
+ #define _ALTF4_ 1 /// ALT_F4 ACTIVE
+
+
+
+
+
+#define MESSAGE_ALT_F4 "Confirm destroy Application"
+
+
+/// ----------------------------------------
+/// par default
+///-----------------------------------------
+#define VTENAME "VTE-TERM5250"
+
+
+
+
+//*******************************************************
+// PROGRAME
+//*******************************************************
+
+
+
+GtkWidget	*window, *terminal;
+AdwDialog *dialog;
+AdwAlertDialog *Alertdialog;
+GPid child_pid = 0;
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+//	function alphanumeric switch
+////////////////////////////////////////////////////////////////////////////////////////////////////
+constexpr unsigned long long int strswitch(const char* c_name, unsigned long long int l_hash = 0)
+{
+	/// string to int for switch
+	return (*c_name == 0) ? l_hash : 101 * strswitch(c_name + 1) + *c_name;
+}
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+//	Contrôle affectation programme
+//////////////////////////////////////////////////////////////////////////////////////////////////
+bool ctrlPgm(std::string v_TEXT)
+{
+	int b_pgm = false;
+
+
+	std::filesystem::path p(v_TEXT.c_str());
+			switch(strswitch(p.stem().c_str()))
+				{
+					case  strswitch("Gencurs")		: b_pgm =true;		break;
+                    case  strswitch("Pcurs")		: b_pgm =true;		break;
+				}
+	return b_pgm;
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////
+///
+///		traitement terminal GTK.
+///
+///////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////
+///-------------------------------------
+/// traitement ALT+F4
+///-------------------------------------
+
+void close_window() {
+
+    gtk_window_destroy(GTK_WINDOW(window));
+
+
+}
+
+
+
+gboolean dialog_cb (AdwAlertDialog *dialog,  GAsyncResult   *result,  GtkWidget   *self)
+{
+  const char *response = adw_alert_dialog_choose_finish (dialog, result);
+
+    if (g_str_equal(response, "no"))
+
+    {
+       // g_print("no\n");
+       return TRUE;
+    }
+
+    if (g_str_equal(response, "yes"))
+
+    {
+        gtk_window_destroy(GTK_WINDOW(self));
+    }
+    return FALSE;
+}
+
+static void showAlert_cb()
+{
+
+    dialog = adw_alert_dialog_new ("confirm destroy Application", NULL);
+
+
+	adw_alert_dialog_set_body (ADW_ALERT_DIALOG (dialog), "Please be careful");
+
+
+    adw_alert_dialog_add_responses (ADW_ALERT_DIALOG (dialog),
+                                  "yes",   "_YES",
+                                  "no",    "_NO",
+                                  NULL);
+
+
+    Alertdialog = ADW_ALERT_DIALOG (dialog);
+    adw_alert_dialog_choose (Alertdialog, GTK_WIDGET (window),
+                           NULL, (GAsyncReadyCallback) dialog_cb, window);
+
+	adw_alert_dialog_set_default_response(  Alertdialog, "no");
+
+
+    adw_dialog_present (dialog, window);
+
+    g_signal_connect(window,"close-request", G_CALLBACK (showAlert_cb), window);
+
+}
+///-------------------------------------
+/// traitement clipboard
+///-------------------------------------
+//gboolean clipboard(GtkWidget *widget)
+//{
+//	return false;
+//}
+/// -----------------------------------------------------------------------------
+/// personalisation projet utilisant une terminal simplifier pour de la gestion
+/// -----------------------------------------------------------------------------
+
+void	init_Terminal()
+{
+
+
+	VteTerminal *VTE;
+
+    #define VTEFONT	"Source code Pro"
+
+
+	gchar * font_terminal = (char*) malloc (50);
+
+	/// confortable and extend numbers columns and rows
+	// HELIX
+
+    unsigned int COL=	132; // 120 cols  src
+    unsigned int ROW =	40;  // 38  lines src
+
+
+    //determines the maximum size for screens
+    Display* d = XOpenDisplay(NULL);
+    gint  s = DefaultScreen(d);
+    // obsolète
+    gint width  = DisplayWidth(d, s);;
+    gint height = DisplayHeight(d, s);
+
+
+    if ( width <= 1600 && height >=1024 ) {                // ex: 13"... 15"
+        g_sprintf(font_terminal,"%s  %s" , VTEFONT,"10");
+        }
+    else if ( width <= 1920 && height >=1080 ) {           // ex: 17"... 32"
+        g_sprintf(font_terminal,"%s  %s" , VTEFONT,"12");
+        }
+    else if ( width > 1920 && width<= 2560  ) {            //  ex: 2560 x1600 > 27"  font 13
+        g_sprintf(font_terminal,"%s  %s" , VTEFONT,"13");
+    }
+    else if ( width > 2560  ) {                            //  ex: 3840 x2160 > 32"  font 14
+        g_sprintf(font_terminal,"%s  %s" , VTEFONT,"13");
+    }
+
+
+	//printf(" font_term  %s  %d\n",font_terminal, g_utf8_validate(font_terminal,-1,NULL));
+
+	// resize  title  font
+	VTE = VTE_TERMINAL (terminal);
+
+
+	vte_terminal_set_font (VTE,pango_font_description_from_string(font_terminal));	/// font use
+
+	vte_terminal_set_size (VTE, COL, ROW);											/// size du terminal
+
+	vte_terminal_set_scrollback_lines (VTE,0);		 								///	désactiver historique.
+
+	vte_terminal_set_scroll_on_output(VTE,FALSE);									/// pas de défilement
+
+	vte_terminal_set_scroll_on_keystroke(VTE,FALSE);								/// pas de défilement
+
+	vte_terminal_set_mouse_autohide(VTE, TRUE);										/// hiden  mouse  keyboard Actif
+
+	vte_terminal_set_cursor_blink_mode(VTE, VTE_CURSOR_BLINK_ON);					/// cursor blink on
+
+	vte_terminal_set_cursor_shape(VTE,VTE_CURSOR_SHAPE_BLOCK);						/// define cursor 'block'
+
+
+}
+
+
+
+/// -----------------------------------------------------------------------------
+/// Callback for vte_terminal_spawn_async    retrived PID terminal ONLY
+/// -----------------------------------------------------------------------------
+void term_spawn_callback(VteTerminal *terminal, GPid pid, GError *error, gpointer user_data)
+{
+        child_pid = pid;
+}
+
+
+/// -----------------------------------------------------------------------------
+/// possibility to change the name of the terminal
+/// -----------------------------------------------------------------------------
+void on_title_changed(GtkWidget *terminal)
+{
+    const char *title;
+    title = vte_terminal_get_termprop_string_by_id(VTE_TERMINAL(terminal), VTE_PROPERTY_ID_XTERM_TITLE, NULL);
+    gtk_window_set_title (GTK_WINDOW(window), title);
+}
+
+/// -----------------------------------------------------------------------------
+/// possibility to change the number of columns and rows
+/// -----------------------------------------------------------------------------
+
+void on_resize_window(GtkWidget *terminal, guint  _col, guint _row)
+{
+	vte_terminal_set_size (VTE_TERMINAL(terminal),_col,_row);
+	gtk_window_set_default_size(GTK_WINDOW(window),-1,-1);
+}
+
+
+/// -----------------------------------------------------------------------------
+///  libvte function putting the terminal function active
+/// -----------------------------------------------------------------------------
+
+inline bool exists_File (const std::string& name) {
+	struct stat fileStat;
+	if(stat(name.c_str(),&fileStat) < 0) return false;  	// is exist objet
+
+	stat(name.c_str(),&fileStat);
+	if (S_ISDIR(fileStat.st_mode) == 1 ) return false;  	// is dir
+
+	if ((fileStat.st_mode & S_IXUSR) == 0 ) return false; 	// pas un executable
+
+	return (stat (name.c_str(), &fileStat) == 0);
+}
+
+
+// programme linux pas d'extention windows ".exe"
+inline bool extention_File(const std::string& name) {
+		std::filesystem::path filePath = name.c_str();
+		if (filePath.extension()!= "") return false;
+		return true;
+}
+
+inline bool isDir_File(const std::string& name) {
+		std::string strdir = std::filesystem::path(name.c_str()).parent_path();
+		if (strdir.empty() ) return false;
+		return true;
+}
+
+
+
+static gchar * Env_File(const std::string& name) {
+        std::string xdir = std::filesystem::path(name.c_str()).parent_path();
+        gchar * dirx = (char*) malloc (100);
+        g_sprintf(dirx,"%s" , xdir.c_str());
+        return dirx;
+}
+
+
+static gchar * Pgm_File(const std::string& name) {
+        std::filesystem::path p(name.c_str());
+        gchar * pgm = (char*) malloc (100);
+        g_sprintf(pgm,"%s" , p.stem().c_str());
+        return pgm;
+}
+
+static gchar * Dir_File(const std::string& name) {
+        std::string xdir = std::filesystem::path(name.c_str());
+        gchar * dirx = (char*) malloc (100);
+        g_sprintf(dirx,"%s" , xdir.c_str());
+        return dirx;
+}
+
+int main(int argc, char *argv[])
+{
+
+    if (argc < 2 || argc > 3)  return EXIT_FAILURE;
+    std::setlocale(LC_ALL, "fr_FR.utf8");;
+    gchar *wrkdir;
+    gchar ** command ;
+
+    // Définir le PATH (ajoute ton chemin au PATH existant)
+    const char *env_path = g_getenv("PATH");
+    char *new_path = g_strdup_printf("PATH=%s:%s", env_path, Env_File(argv[1]));
+
+    // Tableau des variables d'environnement (doit se terminer par NULL)
+    char *envp[] = {
+        new_path,  // Ajoute ton chemin au PATH
+        (gchar*)"TERM=xterm-256color"  // Exemple d'autre variable
+    };
+
+    // definir le PATH de travail
+    wrkdir  = Dir_File(argv[2]);
+
+
+/// -----------------------------------------------------------------------------
+/// -----------------------------------------------------------------------------
+/// -----------------------------------------------------------------------------
+/// contrôle autorisation traitement --> protection
+
+
+    if (argc == 2 )  {
+        if ( false == ctrlPgm((char*)argv[1]))             return EXIT_FAILURE;      // contrôle file autorisation
+        if ( false == extention_File((char*)argv[1]) )     return EXIT_FAILURE;      // contrôle extention
+        if ( false == isDir_File((char*)argv[1]) )         return EXIT_FAILURE;     // contrôle is directory
+        if ( false == exists_File((char*)argv[1]) )        return EXIT_FAILURE;     // contrôle si programme
+        gchar *arg_2[] = { Pgm_File(argv[1]), NULL};
+        command = arg_2;
+    }
+
+
+
+    if (argc == 3 )  {
+        if ( FALSE == ctrlPgm((char*)argv[1]))             return EXIT_FAILURE;    // contrôle file autorisation
+        if ( FALSE == extention_File((char*)argv[1]) )     return EXIT_FAILURE;    // contrôle extention
+        if ( FALSE == isDir_File((char*)argv[1]) )         return EXIT_FAILURE;    // contrôle is directorie
+        if ( FALSE == exists_File((char*)argv[1]) )        return EXIT_FAILURE;    // contrôle si programme
+
+        char * arg_3[] = { Pgm_File(argv[1]),(gchar*)"-c", (gchar*) argv[3], NULL};	    /// arg[2] P_pgm , [2] Pid_msgq
+        command = arg_3;
+    }
+
+
+
+/// -----------------------------------------------------------------------------
+/// -----------------------------------------------------------------------------
+/// -----------------------------------------------------------------------------
+
+
+	// Initialise GTK, the window traditional work
+
+	adw_init ();
+    window = gtk_window_new ();
+
+	gtk_window_set_title(GTK_WINDOW(window),VTENAME);  // name PROJECT
+
+    gtk_window_set_resizable (GTK_WINDOW(window),TRUE);
+
+    gtk_window_set_modal(GTK_WINDOW(window),TRUE);			   // <--- spécifique helix
+
+    #ifdef _ALTF4_
+        gtk_window_set_deletable (GTK_WINDOW(window),true);
+    #else
+        gtk_window_set_deletable (GTK_WINDOW(window),false);
+    #endif
+
+
+	/* Initialise the terminal */
+	terminal = vte_terminal_new();
+
+	// specific initialization of the terminal
+	init_Terminal();
+
+	vte_terminal_spawn_async(
+		VTE_TERMINAL(terminal), //VteTerminal *terminal
+		VTE_PTY_DEFAULT, // VtePtyFlags pty_flags,
+
+		wrkdir,			// const char *working_directory PROJECT ex; $home/myproject/src-zig
+		command,		// command    call pgm and parm
+
+		envp,			// environment
+		(GSpawnFlags)(G_SPAWN_SEARCH_PATH |G_SPAWN_FILE_AND_ARGV_ZERO),		// spawn flags
+		NULL,			// GSpawnChildSetupFunc child_setup,
+		NULL,			// gpointer child_setup_data,
+		NULL,			// GDestroyNotify child_setup_data_destroy,
+		-1,				// int timeout
+		NULL,			// GCancellable *cancellable,
+
+		&term_spawn_callback,   // VteTerminalSpawnAsyncCallback callback, get pid child
+
+		NULL
+    );
+
+    gtk_window_set_child(GTK_WINDOW(window), terminal);
+
+	// Connect some signals
+    g_signal_connect(window,"close-request", G_CALLBACK (showAlert_cb), NULL);
+    g_signal_connect(terminal,"child-exited", G_CALLBACK (close_window), NULL);
+    g_signal_connect(terminal,"resize-window", G_CALLBACK(on_resize_window),NULL);
+	g_signal_connect(terminal, "window-title-changed", G_CALLBACK(on_title_changed), NULL);
+
+
+
+
+    gtk_window_present (GTK_WINDOW (window));
+
+    while (g_list_model_get_n_items (gtk_window_get_toplevels ()) > 0)
+    g_main_context_iteration (NULL, TRUE);
+
+
+
+    return EXIT_SUCCESS;
+}
